17751e8c76df50f42c5914d25c9080f92adbb694
secret515 <41595419+secret515@users.noreply.github.com>
2018-09-28 22:59:41 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,20 +1,24 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
-client.on('guildMemberRemove', Sal => { //By Salto7#4595
-  var embed = new Discord.RichEmbed()
-  .setAuthor(Sal.user.username, Sal.user.avatarURL)
-  .setThumbnail(Sal.user.avatarURL)
-  .setImage('https://previews.123rf.com/images/dxinerz/dxinerz1509/dxinerz150901337/45612790-bye-goodbye-icon.jpg') //هنا حط الصوره الي تبيها
-  .setTitle('خرج عضو')
-  .setDescription('الله معك مع السلامه')
-  .addField('``ايدي العضو``:',"" +  Sal.user.id, true)
-  .addField('``تاق العضو``', Sal.user.discriminator, true)
-  .addField('``تم الانشاء في``', Sal.user.createdAt, true)
-  .addField(' 👤 الان ',`**[ ${Sal.guild.memberCount} ]**`,true)
-  .setColor('RED')
-  .setFooter(Sal.guild.name, Sal.guild.iconURL, true)
-  var channel =Sal.guild.channels.find('name', 'bo7') // هنا حط اسم الروم الي تبيه يكتب فيه
-  if (!channel) return;
-  channel.send({embed : embed});
+client.on('message', message => {
+ if(message.content.startsWith(! + "تعال")) {
+message.member.voiceChannel.join();
+}
 });
+
+==============================
+client.on('message', msg => {
+
+    if (msg.content == '1join') {
+        if (msg.member.voiceChannel) {
+
+     if (msg.member.voiceChannel.joinable) {
+         msg.member.voiceChannel.join().then(msg.react('✅'));
+     }
+    }
+}
+})
+client.on('ready', () => {
+    client.channels.get("ايدي الروم").join();
+    });
